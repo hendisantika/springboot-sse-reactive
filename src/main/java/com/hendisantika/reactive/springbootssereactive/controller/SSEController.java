0b2d02dbabcf5d85fd8e296ec1cc43ffec694364
Hendi Santika <hendi.santika@allianz.co.id>
2019-02-24 12:35:27 +0700

    #4 - Add quotes map

---
+++ src/main/java/com/hendisantika/reactive/springbootssereactive/controller/SSEController.java
@@ -0,0 +1,35 @@
+package com.hendisantika.reactive.springbootssereactive.controller;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sse-reactive
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-24
+ * Time: 12:34
+ */
+
+@Controller
+public class SSEController {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+
+    private Map<Integer, String> quotes = new ConcurrentHashMap<Integer, String>() {
+        {
+            put(0, "Life is what happens to you while you’re busy making other plans. –John Lennon");
+            put(1, "An unexamined life is not worth living. –Socrates");
+            put(2, "The most common way people give up their power is by thinking they don’t have any. –Alice Walker");
+            put(3, "Every child is an artist.  The problem is how to remain an artist once he grows up. –Pablo Picasso");
+            put(4, "Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it. –Johann Wolfgang von Goethe");
+
+        }
+    };
+}
