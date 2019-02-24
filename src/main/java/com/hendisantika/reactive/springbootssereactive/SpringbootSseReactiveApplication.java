19a3020363e5f2be8f78ad835d7afe60771b1c53
Hendi Santika <hendi.santika@allianz.co.id>
2019-02-24 12:31:07 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/reactive/springbootssereactive/SpringbootSseReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.reactive.springbootssereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSseReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSseReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/reactive/springbootssereactive/SpringbootSseReactiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.reactive.springbootssereactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSseReactiveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
